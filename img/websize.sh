b7ea6669ce5e77f1d5f80f561ea0cb9121b9a3cf
David Lilien <dlilien90@gmail.com>
2020-05-15 00:21:03 +0200

    reresize

--- img/websize.sh
+++ img/websize.sh
@@ -7,4 +7,4 @@
 #out=${in%.*}_sm.jpg
 #convert $in -units PixelsPerInch -antialias -resize 1500x1500 -density 150 -quality 85 $out
 
-parallel "convert {} -units PixelsPerInch -antialias -resize 1400x1400 -density 200 -quality 100 {.}.jpg" ::: $@
+parallel "convert {} -units PixelsPerInch -antialias -resize 1900x1200 -density 200 -quality 100 {.}.jpg" ::: $@
